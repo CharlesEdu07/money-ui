dc2f47a750f088747fbda702fcc55047c73b6dc3
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-02 11:47:03 -0300

    Criando o protótipo do formulário de cadastro de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,29 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/components/button/button';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
 
 import { AppComponent } from './app.component';
 import { PostingSearchComponent } from './posting-search/posting-search.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PersonSearchComponent } from './person-search/person-search.component';
+import { PostingRegisterComponent } from './posting-register/posting-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostingSearchComponent,
     NavbarComponent,
-    PersonSearchComponent
+    PersonSearchComponent,
+    PostingRegisterComponent
   ],
   imports: [
     BrowserModule,
     ButtonModule,
     DataTableModule,
     InputTextModule,
+    InputTextareaModule,
     TooltipModule
   ],
   providers: [],
---
+++ src/app/posting-register/posting-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posting-register',
+  templateUrl: './posting-register.component.html',
+  styleUrls: ['./posting-register.component.css']
+})
+export class PostingRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
