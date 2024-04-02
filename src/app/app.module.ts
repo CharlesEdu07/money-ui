f6d9c4028c5bda4280331bc26587daf2d78aef93
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-02 18:11:05 -0300

    Usando Calendar para input de datas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ButtonModule } from 'primeng/components/button/button';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
-import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
 
 import { AppComponent } from './app.component';
@@ -22,11 +24,13 @@ import { PostingRegisterComponent } from './posting-register/posting-register.co
     PostingRegisterComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     ButtonModule,
+    CalendarModule,
     DataTableModule,
-    InputTextModule,
     InputTextareaModule,
+    InputTextModule,
     TooltipModule
   ],
   providers: [],
