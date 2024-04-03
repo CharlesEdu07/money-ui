0c3a3ddf43223bc3a7f1ea2cb71efabb2f066fc7
Charles Eduardo <charleseduardofaria@gmail.com>
2024-04-03 11:23:08 -0300

    Protótipo do formulário de cadastro de pessoa

--- src/app/person-register/person-register.component.ts
+++ src/app/person-register/person-register.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person-register.component.css']
 })
 export class PersonRegisterComponent implements OnInit {
+  postingTypes = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' },
+  ];
+
+  categories = [
+    { label: 'Alimentação', value: 'ALIMENTACAO' },
+    { label: 'Transporte', value: 'TRANSPORTE' },
+  ];
+
+  persons = [
+    { label: 'João', value: '1' },
+    { label: 'Maria', value: '2' },
+    { label: 'José', value: '3' },
+  ];
 
   constructor() { }
 
